a5965aafdd43355ad10ab31cb05ec1d6954ff68e
wadiy <wadiy@amazon.com>
2021-03-21 16:43:34 +0100

    Updated code base

--- src/app.js
+++ src/app.js
@@ -1,13 +1,31 @@
 // Hello World sample app.
 const http = require('http');
-
 const port = 3000;
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+  host     : process.env.RDS_HOSTNAME,
+  user     : process.env.RDS_USERNAME,
+  password : process.env.RDS_PASSWORD,
+  port     : process.env.RDS_PORT
+});
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end(`Hello World. This processor's architecture is ${process.arch} and the current platform is ${process.platform}`);
 });
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+
+  console.log('Connected to database.');
+});
+
+connection.end();
 
 server.listen(port, () => {
   console.log(`Server running on processor architecture ${process.arch}`);
