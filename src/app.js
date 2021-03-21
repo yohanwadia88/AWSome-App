b8216745956cc8145d33478fb512bf7de880e945
wadiy <wadiy@amazon.com>
2021-03-21 20:57:48 +0100

    Updated App and dependencies

--- src/app.js
+++ src/app.js
@@ -1,33 +1,84 @@
-// Hello World sample app.
-const http = require('http');
-const port = 3000;
+const app = require('express')();
 const mysql = require('mysql');
 
-var connection = mysql.createConnection({
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  port     : process.env.RDS_PORT
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json({
+    limit: '8mb'
+})); // support json encoded bodies
+
+// environment variables
+const PORT = process.env.PORT || 3000;
+const HOST = process.env.HOST || '0.0.0.0';
+
+// mysql credentials
+const connection = mysql.createConnection({
+	host: process.env.RDS_HOSTNAME || '172.17.0.2',
+	user: process.env.RDS_USERNAME || 'root',
+	password: process.env.RDS_PASSWORD || 'password',
+	database: process.env.RDS_DATABASE || 'test',
+        port: process.env.RDS_PORT
 });
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end(`Hello World. This processor's architecture is ${process.arch} and the current platform is ${process.platform}`);
+connection.connect((err) => {
+	if (err) {
+		console.error('error connecting mysql: ', err);
+	} else {
+		console.log('mysql connection successful');
+		app.listen(PORT, HOST, (err) => {
+			if (err) {
+				console.error('Error starting  server', err);
+			} else {
+				console.log('server listening at port ' + PORT);
+			}
+		});
+	}
 });
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('Database connection failed: ' + err.stack);
-    return;
-  }
-
-  console.log('Connected to database.');
+// home page
+app.get('/', (req, res) => {
+	res.json({
+		success: true,
+		message: 'Hello World. This processor's architecture is ${process.arch} and the current platform is ${process.platform}'
+	});
 });
 
-connection.end();
+// insert a student into database
+app.post('/add', (req, res) => {
+	const demo = req.body;
+	const query = 'INSERT INTO mytable values(?, ?)';
 
-server.listen(port, () => {
-  console.log(`Server running on processor architecture ${process.arch}`);
+	connection.query(query, [demo.name, demo.color], (err, results, fields) => {
+		if (err) {
+			console.error(err);
+			res.json({
+				success: false,
+				message: 'Error occured'
+			});
+		} else {
+			res.json({
+				success: true,
+				message: 'Successfully added the values!'
+			});
+		}
+	});
 });
 
+// fetch all values 
+app.post('/get', (req, res) => {
+	const query = 'SELECT * FROM mytable';
+    connection.query(query, (err, results, fields) => {
+    	if (err) {
+    		console.error(err);
+    		res.json({
+    			success: false,
+    			message: 'Error occured'
+    		});
+    	} else {
+    		res.json({
+    			success: true,
+    			result: results
+    		});
+    	}
+    });
+});
